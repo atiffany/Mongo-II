f94c93bb9800cf020cb92288b8b55633ba58eeec
atiffany <abby.tiffany@gmail.com>
2018-01-10 16:07:26 -0800

    passing two test

--- src/server.js
+++ src/server.js
@@ -11,5 +11,34 @@ const server = express();
 server.use(bodyParser.json());
 
 // TODO: write your route handlers here
+server.get('/users', (req, res) => {
+  Post.find({})
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(401).json({ error: 'Cannot find users' });
+    });
+});
+
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.findOne({ soID })
+    .then((question) => {
+      const newID = question.acceptedAnswerID;
+      Post.findOne({ soID: newID })
+        .then((answer) => {
+          res.status(200).json(answer);
+        })
+        .catch((error) => {
+          res.status(401).json({ error: 'No answer found' });
+        });
+    })
+    .catch((error) => {
+      res.status(401).json({ error: 'No question found' });
+    });
+});
+
+// get.server()
 
 module.exports = { server };
